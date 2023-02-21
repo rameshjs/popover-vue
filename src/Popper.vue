f818ffe95dc49775119a5d605f41375dd8c0376c
Ramesh Kumar <rameshkumar.exe@gmail.com>
2023-02-21 20:17:39 +0530

    Place popper content and arrow to target

--- src/Popper.vue
+++ src/Popper.vue
@@ -3,6 +3,10 @@
     <div class="popper-trigger" ref="popperTrigger">
       <slot></slot>
     </div>
+    <div ref="popperContent" class="popper-content">
+      sadasdasdasdasdasdasdasdasdasdasdasdasdas
+      <div ref="popperArrow" class="arrow"></div>
+    </div>
   </div>
 </template>
 <script>
@@ -11,11 +15,52 @@ export default {
 };
 </script>
 <script setup>
+import { computePosition, offset, flip, shift, arrow } from "@floating-ui/dom";
 import { onMounted, ref } from "vue";
 
 const popperTrigger = ref(null);
+const popperContent = ref(null);
+const popperArrow = ref(null);
+
+const contentPosition = (target) => {
+  const content = popperContent.value;
+  const arrowElement = popperArrow.value;
+
+  computePosition(target, content, {
+    placement: "top",
+    middleware: [
+      offset(6),
+      flip(),
+      shift({ padding: 5 }),
+      arrow({ element: arrowElement }),
+    ],
+  }).then(({ x, y, placement, middlewareData }) => {
+    Object.assign(content.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    });
+
+    const { x: arrowX, y: arrowY } = middlewareData.arrow;
+
+    const staticSide = {
+      top: "bottom",
+      right: "left",
+      bottom: "top",
+      left: "right",
+    }[placement.split("-")[0]];
+
+    Object.assign(arrowElement.style, {
+      left: arrowX != null ? `${arrowX}px` : "",
+      top: arrowY != null ? `${arrowY}px` : "",
+      right: "",
+      bottom: "",
+      [staticSide]: "-4px",
+    });
+  });
+};
 
 const clickEvent = (e) => {
+  contentPosition(e.target);
   console.log(e);
 };
 
